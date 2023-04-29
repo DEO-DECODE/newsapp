8eafeb31fbaedeffcf34098e54e605a4399b8a24
DEO-DECODE <d.jrajsingh81@gmail.com>
2023-04-29 18:42:59 +0530

    till video 28

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -5,16 +5,25 @@ export default class NewsItem extends Component {
     let { title, description, imageUrl, newsUrl } = this.props;
 
     return (
-      <div className="my-3"> 
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://static.vecteezy.com/system/resources/previews/000/197/882/original/vector-news-headlines-background-with-earth-planet.jpg"
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
             <a
+              rel="noreferrer"
               href={newsUrl}
               target="_blank"
-              className="btn btn-sm btn-primary"
+              className="btn btn-sm btn-dark"
             >
               Read More
             </a>
